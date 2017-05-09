7dc2f72cf009963a96e33c5cf38e790a56631600
Perillo Santana <perillo.santana@quatrodigital.com.br>
2017-05-09 10:00:26 -0300

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var watch = require('gulp-watch');
 
 gulp.task('teste', function(){
 	console.log("teste de task do GulpJS");
@@ -9,4 +10,12 @@ gulp.task('stylus', function(){
   gulp.src('src/stylus/**/**.styl')
     .pipe(stylus())
     .pipe(gulp.dest('build/css'))
-})
+});
+
+// Rerun the task when a file changes
+// gulp.task('watch', function() {
+//   gulp.watch(paths.stylus, ['stylus']);
+//   gulp.watch(paths.teste, ['teste']);
+// });
+
+gulp.task('default', ['teste', 'stylus']);
